35a557bc138cd06d2e382884b6ad2e071316dc0a
Ashish Jacob Benson <160376103+skyxwalker@users.noreply.github.com>
2026-02-01 02:19:43 +0530

    delete

--- README.md
+++
@@ -1,1 +0,0 @@
-# Food-Stall-ERP-System
--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://vgiycnneceiwqqopcyki.supabase.co';
-const supabaseAnonKey = 'sb_publishable_MBWpKXwJ4qsOyi4SGVhd0Q_828YDP7k';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
